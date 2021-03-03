c3f5f52379ceb144c48f57c761e008cbc4a3d647
alamdar114 <alamdar114@gmail.com>
2021-03-03 10:28:42 -0600

    adjusted the units of parameters

--- finite_difference.py
+++ finite_difference.py
@@ -14,12 +14,12 @@ def single_trial(dt, gamma, n_0, sigma, dW):
     n[0] = n_0
     for h in range(len(dW)):
         n[h+1] = n[h] + 1* (-gamma*n[h]*dt + sigma*dW[h])
-        if n[h+1]<=0:
-           break
+        # if n[h+1]<=0:
+        #    break
     return n
 
 
-def parallel_finite_difference(n_0=2, nsteps=10000, dt=0.1, trials=1000, gamma=0.15, sigma=(0.0025)**0.5):
+def parallel_finite_difference(n_0=2, nsteps=10000, dt=0.1, trials=1000, gamma=0.015/0.6582, sigma=(0.0025)**0.5):
     '''parallelized version for computing the solution of an SDE using finite difference method
     this function computes the numeric intergral of a stochastic process through the first order approximation of a
      taylor expansion
@@ -29,8 +29,8 @@ def parallel_finite_difference(n_0=2, nsteps=10000, dt=0.1, trials=1000, gamma=0
     trials are the number of times a simulation is repeated for the same starting value
     nsteps are the number of time steps
     dt is the size of time step
-    gamma is the decay rate
-    sigma is the sqrt of variance'''
+    gamma is the decay rate in 1/fs
+    sigma is the sqrt of variance in 1/fs'''
     with concurrent.futures.ProcessPoolExecutor() as executor:
         t = np.linspace(0.0, nsteps * dt, nsteps + 1)
         dW = (dt**0.5) * random.normal(0,1,(trials, nsteps))
@@ -49,7 +49,7 @@ def parallel_finite_difference(n_0=2, nsteps=10000, dt=0.1, trials=1000, gamma=0
         return t,n
 
 
-def finite_difference_method(n_0=2, nsteps=1000, dt=0.1, trials=100, gamma=0.15, sigma=(0.0025)**0.5):
+def finite_difference_method(n_0=2, nsteps=1000, dt=0.1, trials=100, gamma=0.15/0.6582, sigma=(0.0025)**0.5):
     """ this function computes the numeric intergral of a stochastic process through the first order approximation of a
      taylor expansion
     u((i+1)h) = u(ih) + h*u'(ih)
@@ -58,8 +58,8 @@ def finite_difference_method(n_0=2, nsteps=1000, dt=0.1, trials=100, gamma=0.15,
     trials are the number of times a simulation is repeated for the same starting value
     nsteps are the number of time steps
     dt is the size of time step
-    gamma is the decay rate
-    sigma is the sqrt of variance
+    gamma is the decay rate in 1/fs
+    sigma is the sqrt of variance in 1/fs
     """
     dW = (dt ** 0.5) * random.normal(0, 1, (trials, nsteps))
     n = np.ones((trials, nsteps+1))*1
@@ -212,24 +212,30 @@ def single_spec(hbar, mu, ng,  w0, V0, t, phiN1):
     s1 = np.zeros((len(phiN1)), dtype=complex)  # container for the first order response
     c1 = 2 * (mu ** 2) / hbar
     for k in range(len(s1)):
-        c2 = cmath.exp((+1j / hbar) * (t[k] * (w0 + V0 * ng) + 2 * V0 * phiN1[k]))
+        c2 = cmath.exp(1j * (t[k] * (w0 + V0 * ng) + 2 * V0 * phiN1[k]))
         c3 = (cmath.exp(-1j * V0 * t[k]) - 1) * ng - 1
         c = c2 * c3
         s1[k] = -c1 * c.imag
     return s1
 
 
-def first_order_spec(parallel=True, n_0=2):
+def first_order_spec(dt=0.1, nsteps=100000, gamma=0.010, sigma=0.0025 ** 0.5, sigmaN0=0.125 ** 0.5, N0=2, trials=1000 ,parallel=True):
     '''main function for computing the first order spectrum through finite element solution of SDE.
+    Inputs
+    dt = time step,
+    nsteps = number of time-steps,
+    gamma = decay rate given in eV
+    sigma = brownian std in 1/fs
+    sigmaN0 = laser std or initial exciton std without any units
+    V0 = interaction fixed at (10 meV/1000hbar) in 1/fs
+    N0 = mean of initial population for k!=0 excitons
+    trials = number of trajectories to run
     Returns an averaged spectrum for a single starting volue of the N(0)'''
-    trials = 1000 # number of trajectories to run
-    dt = 0.1
-    nsteps = 100000
+
+    n_0 = N0 # initial value of the k!=0 exciton population
     hbar = 0.6582  # eV.fs
-    gamma = 10  # meV
-    gamma = gamma / (hbar * 1000)  # per fs
+    gamma = gamma / hbar  # per fs
     sigma = 0.0025 ** 0.5  # per fs
-    n_0 = 2  # initial value of the k!=0 exciton population
     ng = 1  # the ground state population of k=0 excitons
     if parallel:
         t,n = parallel_finite_difference(n_0, nsteps, dt, trials, gamma, sigma)
@@ -238,9 +244,8 @@ def first_order_spec(parallel=True, n_0=2):
     phi_n = np.zeros(n.shape)
     phi_n[:, 1:] = dt * np.cumsum(n[:, :nsteps], axis=-1)
 
-    V0 = 0.010  # the interaction potential in eV
-    w0 = 2.35  # the frequency in energy units eV
-    hbar = 0.658  # eV.fs
+    V0 = 0.010/hbar  # the interaction potential in 1/fs
+    w0 = 2.35/hbar  # the frequency 1/fs
     mu = 1 # dipole moment in some arb units.
     c1 = 2 * (mu ** 2) / hbar
     if parallel:
@@ -255,41 +260,47 @@ def first_order_spec(parallel=True, n_0=2):
         s1 = np.zeros((trials, nsteps + 1), dtype=complex) #container for the first order response
         for m in range(trials):
             for k in range(nsteps + 1):
-                c2 = cmath.exp((+1j / hbar) * (t[k] * (w0 + V0 * ng) + 2 * V0 * phi_n[m, k]))
+                c2 = cmath.exp(1j * (t[k] * (w0 + V0 * ng) + 2 * V0 * phi_n[m, k]))
                 c3 = (cmath.exp(-1j*V0*t[k])-1)*ng - 1
                 c = c2*c3
                 s1[m, k] = -c1*c.imag
 
-    s1_mean = np.mean(s1, 0)
-    s1dummy = np.ones(nsteps + 1, dtype=complex)  # spectrum without broadening
+    s1_mean = np.mean(s1, 0) # spectrum with broadening and shift
+    s1dummy = np.ones(nsteps + 1, dtype=complex)  # spectrum without broadening but with shift
     s1dummy2 = np.ones(nsteps + 1, dtype=complex)  # spectrum without broadening and without shift
     for k in range(nsteps + 1):
-        c2 = cmath.exp((+1j / hbar) * (t[k] * (w0 + V0 * ng) + 2 * V0 * 0))
+        c2 = cmath.exp(1j * (t[k] * (w0 + V0 * ng) + 2 * V0 * 0))
         c3 = (cmath.exp(-1j*V0*t[k])-1)*ng - 1
         c = c2*c3
         s1dummy[k] = -c1*c.imag
-        s1dummy2[k] = c1*(cmath.exp(+1j*t[k]*w0/hbar)).imag
+        s1dummy2[k] = c1*(cmath.exp(+1j*t[k]*w0)).imag
 
+    # plt.figure()
+    # plt.plot(t, s1_mean)
+    # #plt.plot(t, s1dummy)
+    # #plt.plot(t, s1dummy2)
+    # #plt.show()
+    # plt.show()
+
+    S1 = np.fft.fft(s1_mean) / len(t)
+    freq = np.fft.fftfreq(len(S1), d=dt)
+    energy = freq * np.pi * 2 * hbar
+
+    S1_dummy = np.fft.fft(s1dummy) / len(t)
+    S1_dummy2 = np.fft.fft(s1dummy2) / len(t)
+
+
+    limit = int(len(t)/2)
     plt.figure()
-    plt.plot(t, s1_mean)
-    #plt.plot(t, s1dummy)
-    #plt.plot(t, s1dummy2)
-    #plt.show()
-
-    spec_s1 = fftpack.fftshift(fftpack.fft(s1_mean))
-    spec_s1dummy = fftpack.fftshift(fftpack.fft(s1dummy))
-    spec_s1dummy2 = fftpack.fftshift(fftpack.fft(s1dummy2))
-    freq = fftpack.fftshift(fftpack.fftfreq(t.shape[-1], dt)) * (2 * np.pi)*hbar
-
-    plt.figure()
-    plt.plot(freq, abs(spec_s1dummy2))
-    plt.plot(freq, abs(spec_s1dummy))
-    plt.plot(freq, abs(spec_s1))
+    plt.plot(energy[:limit], abs(S1_dummy2[:limit]))
+    plt.plot(energy[:limit], abs(S1_dummy[:limit]))
+    plt.plot(energy[:limit], abs(S1[:limit]))
     plt.xlabel('Energy (eV)')
     plt.legend(['Original', 'Interacting', 'Stochastic'])
     plt.show()
+    return t[:limit], S1[:limit]
 
-def first_order_exact(dt=0.1, nsteps=100000, gamma=0.012, sigma=0.0025 ** 0.5, sigmaN0=0.125 ** 0.5, N0=2):
+def first_order_exact(dt=0.1, nsteps=100000, gamma=0.010, sigma=0.0025 ** 0.5, sigmaN0=0.125 ** 0.5, N0=2):
     '''Exact formulation of the first order response as calculated by Hao et. al.
     time is in fs, rate 1/fs, frequencies or energies in eV
 
@@ -298,7 +309,7 @@ def first_order_exact(dt=0.1, nsteps=100000, gamma=0.012, sigma=0.0025 ** 0.5, s
     nsteps = number of time-steps,
     gamma = decay rate given in eV
     sigma = brownian std in 1/fs
-    sigmaN0 = laser std or initial exciton std. NOTE: it is unit less
+    sigmaN0 = laser std or initial exciton std without any units
     V0 = interaction fixed at (10 meV/1000hbar) in 1/fs
     N0 = mean of initial population for k!=0 excitons
         Returns the spectrum and frequencies'''
@@ -318,9 +329,8 @@ def first_order_exact(dt=0.1, nsteps=100000, gamma=0.012, sigma=0.0025 ** 0.5, s
                                 for t in time]
     S1 = np.fft.fft(s1) / len(time)
     freq = np.fft.fftfreq(len(S1), d=dt)
-    energy = 2*np.pi*hbar*freq
 
-
+    energy = freq*np.pi*2*hbar
     # plt.figure()
     # plt.plot(time, s1)
     # plt.title('First order response in time domain')
@@ -330,10 +340,10 @@ def first_order_exact(dt=0.1, nsteps=100000, gamma=0.012, sigma=0.0025 ** 0.5, s
     limit = int(nsteps/2)
     # print(limit)
     # plt.figure()
-    # plt.plot(energy[:limit], np.abs(S1[:limit]))
+    # plt.plot(2*np.pi*hbar*freq[:limit], np.abs(S1[:limit]))
     # plt.title('Spectrum of first order response')
     # plt.xlabel('Energy or Freq. (eV)')
     # plt.ylabel('S1(w)')
     # plt.show()
 
-    return energy[:limit], np.abs(S1[:limit])
+    return energy[:limit],np.abs(S1[:limit])
